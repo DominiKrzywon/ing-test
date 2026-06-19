cc317e362645435495d13c594ebd0d971257f01a
Dominik <dominikkrzywon@gmail.com>
2026-06-19 14:02:52 +0200

    Refactor: update retries in Playwright config to 1 for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   expect: { timeout: 10 * 1000 },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   reporter: [["html", { open: "never" }], ["list"]],
   use: {
     baseURL: "https://www.ing.pl",
